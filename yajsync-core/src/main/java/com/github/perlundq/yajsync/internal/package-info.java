9dbcfc47a8ee2d09f81a1218a5e237df88e55585
Per Lundqvist <perlundq@gmail.com>
2016-07-05 23:29:05 +0200

    Add info about com.github.perlundq.yajsync.internal not being part of the external API
    Make it clear that everything below
    com.github.perlundq.yajsync.internal is not part of the public
    API. All the packages and classes below
    com.github.perlundq.yajsync.internal are internal to yajsync only and
    is likely to change in the future.

---
+++ yajsync-core/src/main/java/com/github/perlundq/yajsync/internal/package-info.java
@@ -0,0 +1,5 @@
+/**
+   Warning: all packages and classes below com.github.perlundq.yajsync.internal
+   belong to yajsync's internal API only - do not use.
+ */
+package com.github.perlundq.yajsync.internal;
