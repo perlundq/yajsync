perlundq/yajsync

commit 71af3272e425c3e46b6871f2965c05af10797f7f

FileInfo: remove the Path and add LocatatableFileInfo + more...
Remove the requirement for having a Path in the file information in
rsync file listings (which did not make sense having). Make the byte
representation non-public. Create a specific interface
(LocatableFileInfo) for file information containing a Path.

Details:
- Change representation from class to interface for FileInfo,
  DeviceInfo, SymlinkInfo. These are not meant to be implemented by
  the API user.
- Add LocatableFileInfo representing FileInfo which has a Path. Sender
  always uses LocatableFileInfo, Receiver/Generator also uses it
  unless it's a file/module listing or some error occurs when deriving
  the Path.
- FileInfo: do not expose the byte representation externally.
- FileInfo: remove field _normalizedPath
- FileInfo: add pathName() which returns the textual representation of
  the path.
- FileInfo: derive equals/hashCode from the byte representation rather
  than the normalized path name (NOTE: also removing the ability to be
  able to regard certain files as identical when having differing byte
  representations but identical normalized path names, previously used
  for avoiding several file transfers of identical files on windows
  file systems).
- FileInfo: do not automatically add a trailing slash for
  directories. Modify (and simplify) implementation of compareTo
  accordingly.
- change package com.github.perlundq.yajsync.internal.filelist ->
  com.github.perlundq.yajsync.internal.session for FileInfoTest and
  file lists to be able use package private methods in FileInfoImpl

yajsync-core/src/main/java/com/github/perlundq/yajsync/internal/session/FileInfoImpl.java
@@ -1,0 +1,168 @@
+/*
+ * Copyright (C) 1996-2011 by Andrew Tridgell, Wayne Davison, and others
+ * Copyright (C) 2013-2016 Per Lundqvist
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.github.perlundq.yajsync.internal.session;
+
+import java.util.Arrays;
+
+import com.github.perlundq.yajsync.attr.FileInfo;
+import com.github.perlundq.yajsync.attr.RsyncFileAttributes;
+import com.github.perlundq.yajsync.internal.text.Text;
+
+// NOTE: all internal FileInfo objects are instances of FileInfoImpl
+class FileInfoImpl implements FileInfo
+{
+    // _pathNameOrNull may only be null internally in Receiver, any such
+    // instance will never be exposed externally
+    private final String _pathNameOrNull;
+    private final byte[] _pathNameBytes;
+    private final RsyncFileAttributes _attrs;
+
+    FileInfoImpl(String pathNameOrNull, byte[] pathNameBytes,
+                 RsyncFileAttributes attrs)
+    {
+        assert pathNameBytes != null;
+        assert attrs != null;
+        assert pathNameBytes.length > 0;
+        assert pathNameBytes[0] != Text.ASCII_SLASH;
+        assert !isDotDir(pathNameBytes) || attrs.isDirectory();
+        assert pathNameBytes[pathNameBytes.length - 1] != Text.ASCII_SLASH;
+
+        _pathNameOrNull = pathNameOrNull;
+        _pathNameBytes = pathNameBytes;
+        _attrs = attrs;
+    }
+
+    @Override
+    public String toString()
+    {
+        String str = _pathNameOrNull == null
+                ? "untransferrable " + Text.bytesToString(_pathNameBytes)
+                : _pathNameOrNull;
+        return String.format("%s (%s)", getClass().getSimpleName(), str);
+    }
+
+    @Override
+    public boolean equals(Object obj)
+    {
+        // It is OK - FileInfo is not meant to be implemented by the API end
+        // user and all our FileInfo implementing classes extends FileInfoImpl.
+        if (obj instanceof FileInfoImpl) {
+            FileInfoImpl other = (FileInfoImpl) obj;
+            return Arrays.equals(_pathNameBytes, other._pathNameBytes);
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return Arrays.hashCode(_pathNameBytes);
+    }
+
+    @Override
+    public int compareTo(FileInfo otherFileInfo)
+    {
+        FileInfoImpl other = (FileInfoImpl) otherFileInfo;
+        int result = compareUnixFileNamesBytes(_pathNameBytes,
+                                               _attrs.isDirectory(),
+                                               other._pathNameBytes,
+                                               other._attrs.isDirectory());
+        assert result != 0 || this.equals(other);
+        return result;
+    }
+
+    @Override
+    public RsyncFileAttributes attrs()
+    {
+        return _attrs;
+    }
+
+    @Override
+    public String pathName()
+    {
+        return _pathNameOrNull;
+    }
+
+    boolean isDotDir()
+    {
+        return isDotDir(_pathNameBytes);
+    }
+
+    private static boolean isDotDir(byte[] bytes)
+    {
+        return bytes.length == 1 && bytes[0] == Text.ASCII_DOT;
+    }
+
+    /*
+     * sort . (always a dir) before anything else
+     * sort files before dirs
+     * compare dirs using a trailing slash
+     */
+    private static int compareUnixFileNamesBytes(byte[] leftBytes,
+                                                 boolean isLeftDir,
+                                                 byte[] rightBytes,
+                                                 boolean isRightDir)
+    {
+        if (isDotDir(leftBytes)) {
+            if (isDotDir(rightBytes)) {
+                return 0;
+            }
+            return -1;
+        } else if (isDotDir(rightBytes)) {
+            return 1;
+        }
+
+        if (isLeftDir != isRightDir) {
+            if (isLeftDir) {
+                return 1;
+            }
+            return -1;
+        }
+
+        int i = 0;
+        for (; i < leftBytes.length && i < rightBytes.length; i++) {
+            int diff = leftBytes[i] - rightBytes[i];
+            if (diff != 0) {
+                return diff;
+            }
+        }
+        // one or both are at the end
+        // one or both are a substring of the other
+        // either both are directories or none is
+        boolean isLeftAtEnd = i == leftBytes.length;
+        boolean isRightAtEnd = i == rightBytes.length;
+
+        if (isLeftDir) { // && isRightDir
+            if (isLeftAtEnd && isRightAtEnd) {
+                return 0;
+            } else if (isLeftAtEnd) {
+                return Text.ASCII_SLASH - rightBytes[i];
+            } else if (isRightAtEnd) {
+                return leftBytes[i] - Text.ASCII_SLASH;
+            }
+        }
+
+        if (isLeftAtEnd == isRightAtEnd) {
+            return 0;
+        } else if (isLeftAtEnd) {
+            return -1;
+        }
+        return 1;
+    }
+}

yajsync-core/src/test/java/com/github/perlundq/yajsync/internal/session/FileInfoTest.java
@@ -1,0 +1,108 @@
+/*
+ * Copyright (C) 2016 Per Lundqvist
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.github.perlundq.yajsync.internal.session;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import com.github.perlundq.yajsync.attr.FileInfo;
+import com.github.perlundq.yajsync.attr.Group;
+import com.github.perlundq.yajsync.attr.RsyncFileAttributes;
+import com.github.perlundq.yajsync.attr.User;
+import com.github.perlundq.yajsync.internal.util.FileOps;
+
+public class FileInfoTest {
+
+    RsyncFileAttributes _fileAttrs =
+            new RsyncFileAttributes(FileOps.S_IFREG | 0644, 0, 0, User.JVM_USER,
+                                    Group.JVM_GROUP);
+    RsyncFileAttributes _dirAttrs =
+        new RsyncFileAttributes(FileOps.S_IFDIR | 0755, 0, 0, User.JVM_USER,
+                                Group.JVM_GROUP);
+    String _dotDirPathName = ".";
+    FileInfoImpl _dotDir = new FileInfoImpl(".", ".".getBytes(), _dirAttrs);
+
+    @Test
+    public void testSortDotDirWithDotDirEqual()
+    {
+        assertTrue(_dotDir.equals(_dotDir));
+        assertEquals(_dotDir.compareTo(_dotDir), 0);
+    }
+
+    @Test
+    public void testSortDotDirBeforeNonDotDir()
+    {
+        String str = ".a";
+        FileInfo f = new FileInfoImpl(str, str.getBytes(), _fileAttrs);
+        assertFalse(_dotDir.equals(f));
+        assertTrue(_dotDir.compareTo(f) == -1);
+    }
+
+    @Test
+    public void testSortDotDirBeforeNonDotDir2()
+    {
+        String str = "...";
+        FileInfoImpl f = new FileInfoImpl(str, str.getBytes(), _fileAttrs);
+        assertFalse(_dotDir.equals(f));
+        assertTrue(_dotDir.compareTo(f) == -1);
+    }
+
+    @Test
+    public void testSortDotDirBeforeNonDotDir3()
+    {
+        String str = "...";
+        FileInfoImpl f = new FileInfoImpl(str, str.getBytes(), _dirAttrs);
+        assertFalse(_dotDir.equals(f));
+        assertTrue(_dotDir.compareTo(f) == -1);
+    }
+
+    // test empty throws illegalargumentexception
+    // test dot is always a directory
+
+    /*
+     * ./
+     * ...
+     * ..../
+     * .a
+     * a.
+     * a..
+     * ..a
+     * .a.
+     *
+     * a.
+     * a../
+     * .a/
+     * .a/b
+     * b/
+     * b/.
+     * c
+     * cc
+     * c.c
+     */
+
+    @Test
+    public void testSortDotDirBeforeNonDotDir4()
+    {
+        String str = "a.";
+        FileInfoImpl f = new FileInfoImpl(str, str.getBytes(), _dirAttrs);
+        assertFalse(_dotDir.equals(f));
+        assertTrue(_dotDir.compareTo(f) == -1);
+    }
+}

yajsync-core/src/test/java/com/github/perlundq/yajsync/filelist/FileInfoTest.java
@@ -1,102 +1,0 @@
-package com.github.perlundq.yajsync.filelist;
-
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
-import org.junit.Test;
-
-import com.github.perlundq.yajsync.attr.FileInfo;
-import com.github.perlundq.yajsync.attr.Group;
-import com.github.perlundq.yajsync.attr.RsyncFileAttributes;
-import com.github.perlundq.yajsync.attr.User;
-import com.github.perlundq.yajsync.internal.util.FileOps;
-
-public class FileInfoTest {
-
-    RsyncFileAttributes _fileAttrs =
-        new RsyncFileAttributes(FileOps.S_IFREG | 0644,
-                                0, 0, User.JVM_USER, Group.JVM_GROUP);
-    RsyncFileAttributes _dirAttrs =
-        new RsyncFileAttributes(FileOps.S_IFDIR | 0755,
-                                0, 0, User.JVM_USER, Group.JVM_GROUP);
-    Path _dotDirPath = Paths.get("./");
-    Path dotDirAbsPath = Paths.get("/path/to/module/root/");
-    FileInfo _dotDir = new FileInfo(dotDirAbsPath,
-                                    _dotDirPath,
-                                    _dotDirPath.toString().getBytes(),
-                                    _dirAttrs);
-
-    @Test
-    public void testSortDotDirWithDotDirEqual()
-    {
-        assertTrue(_dotDir.equals(_dotDir));
-        assertTrue(_dotDir.compareTo(_dotDir) == 0);
-    }
-
-    @Test
-    public void testSortDotDirBeforeNonDotDir()
-    {
-        Path p = Paths.get(".a");
-        FileInfo f = new FileInfo(_dotDirPath.resolve(p), p,
-                                  p.toString().getBytes(), _fileAttrs);
-        assertFalse(_dotDir.equals(f));
-        assertTrue(_dotDir.compareTo(f) == -1);
-    }
-
-    @Test
-    public void testSortDotDirBeforeNonDotDir2()
-    {
-        Path p = Paths.get("...");
-        FileInfo f = new FileInfo(_dotDirPath.resolve(p), p,
-                                  p.toString().getBytes(), _fileAttrs);
-        assertFalse(_dotDir.equals(f));
-        assertTrue(_dotDir.compareTo(f) == -1);
-    }
-
-    @Test
-    public void testSortDotDirBeforeNonDotDir3()
-    {
-        Path p = Paths.get("...");
-        FileInfo f = new FileInfo(_dotDirPath.resolve(p), p,
-                                  p.toString().getBytes(), _dirAttrs);
-        assertFalse(_dotDir.equals(f));
-        assertTrue(_dotDir.compareTo(f) == -1);
-    }
-
-    // test empty throws illegalargumentexception
-    // test dot is always a directory
-
-    /*
-     * ./
-     * ...
-     * ..../
-     * .a
-     * a.
-     * a..
-     * ..a
-     * .a.
-     *
-     * a.
-     * a../
-     * .a/
-     * .a/b
-     * b/
-     * b/.
-     * c
-     * cc
-     * c.c
-     */
-
-    @Test
-    public void testSortDotDirBeforeNonDotDir4()
-    {
-        Path p = Paths.get("a.");
-        FileInfo f = new FileInfo(_dotDirPath.resolve(p), p,
-                                  "a./".getBytes(), _dirAttrs);
-        assertFalse(_dotDir.equals(f));
-        assertTrue(_dotDir.compareTo(f) == -1);
-    }
-}

yajsync-app/src/main/java/com/github/perlundq/yajsync/ui/YajSyncClient.java
@@ -848,14 +848,6 @@ public class YajSyncClient
     private String fileInfoToListingString(FileInfo f)
     {
         RsyncFileAttributes attrs = f.attrs();
-        String pathName = _characterDecoder.decodeOrNull(f.pathNameBytes());
-        if (pathName == null) {
-            pathName = String.format("%s <WARNING filename contains " +
-                                     "undecodable characters (using %s)>",
-                                     new String(f.pathNameBytes(),
-                                                _characterDecoder.charset()),
-                                     _characterDecoder.charset());
-        }
         Date t = new Date(FileTime.from(attrs.lastModifiedTime(),
                                         TimeUnit.SECONDS).toMillis());
         if (f instanceof SymlinkInfo) {
@@ -863,12 +855,13 @@ public class YajSyncClient
                                  FileOps.modeToString(attrs.mode()),
                                  attrs.size(),
                                  _timeFormatter.format(t),
-                                 pathName, ((SymlinkInfo) f).targetPath());
+                                 f.pathName(),
+                                 ((SymlinkInfo) f).targetPathName());
         }
         return String.format("%s %11d %s %s",
-                FileOps.modeToString(attrs.mode()),
-                attrs.size(),
-                _timeFormatter.format(t),
-                pathName);
+                             FileOps.modeToString(attrs.mode()),
+                             attrs.size(),
+                             _timeFormatter.format(t),
+                             f.pathName());
     }
 }

yajsync-core/src/main/java/com/github/perlundq/yajsync/attr/DeviceInfo.java
@@ -1,6 +1,4 @@
 /*
- * device file information
- *
  * Copyright (C) 2016 Per Lundqvist
  *
  * This program is free software: you can redistribute it and/or modify
@@ -18,31 +16,8 @@
  */
 package com.github.perlundq.yajsync.attr;
 
-import java.nio.file.Path;
-
-public class DeviceInfo extends FileInfo
+public interface DeviceInfo extends FileInfo
 {
-    private final int _major;
-    private final int _minor;
-
-    public DeviceInfo(Path pathOrNull, Path normalizedPathOrNull,
-                      byte[] pathNameBytes, RsyncFileAttributes attrs,
-                      int major, int minor)
-    {
-        super(pathOrNull, normalizedPathOrNull, pathNameBytes, attrs);
-        assert major >= 0;
-        assert minor >= 0;
-        _major = major;
-        _minor = minor;
-    }
-
-    public int major()
-    {
-        return _major;
-    }
-
-    public int minor()
-    {
-        return _minor;
-    }
+    int major();
+    int minor();
 }

yajsync-core/src/main/java/com/github/perlundq/yajsync/attr/FileInfo.java
@@ -1,8 +1,5 @@
 /*
- * Rsync file information
- *
- * Copyright (C) 1996-2011 by Andrew Tridgell, Wayne Davison, and others
- * Copyright (C) 2013-2015 Per Lundqvist
+ * Copyright (C) 2016 Per Lundqvist
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -19,234 +16,24 @@
  */
 package com.github.perlundq.yajsync.attr;
 
-import java.nio.file.Path;
-import java.util.Objects;
-
-import com.github.perlundq.yajsync.internal.text.Text;
-
 /**
- * a class for rsync file information, we must keep the path name as a string
- * as opposed to storing the Path directly since the latter may fail
- * (when being the receiver) and we must keep the file list identical to the
- * peer's file list when being receiver
+ * A class containing generic file information for any type of file (regular,
+ * directory, symbolic link or device file).
+ *
+ * Warning: not designed to be implemented or extended externally.
+ *
+ * Use attrs() to determine the actual file type. If possible, a more specific
+ * subtype will be used, representing symbolic links (SymlinkInfo) and device
+ * files (DeviceInfo) respectively. But in some circumstances a symbolic link or
+ * device file might be represented as a regular FileInfo - if it is a symlink
+ * and lacks information about symlink target or if it is a device file and
+ * lacks information about major and minor number.
+ *
+ * Also, if the file has a local path representation, an as specific as possible
+ * instance of LocatableFileInfo will be used.
  */
-public class FileInfo implements Comparable<FileInfo>
+public interface FileInfo extends Comparable<FileInfo>
 {
-    private final Path _pathOrNull;                                             // full path to file
-    private final Path _normalizedPathOrNull;                                   // normalized relative path to receiver destination directory
-    private final byte[] _pathNameBytes;                                        // name of relative path to receiver destination (in bytes)
-    private final RsyncFileAttributes _attrs;
-
-    /**
-     * @throws IllegalArgumentException if file has a trailing slash but is not
-     *         a directory
-     */
-    public FileInfo(Path pathOrNull, Path normalizedPathOrNull,
-                    byte[] pathNameBytes, RsyncFileAttributes attrs)
-    {
-        assert pathNameBytes != null && pathNameBytes.length > 0;
-        assert attrs != null;
-
-        boolean isTrailingSlash =
-            pathNameBytes[pathNameBytes.length - 1] == Text.ASCII_SLASH;
-        if (isTrailingSlash && !attrs.isDirectory()) {
-            throw new IllegalArgumentException(String.format(
-                "%s has a trailing slash but is not a directory (path=%s, " +
-                "attrs=%s)", Text.bytesToString(pathNameBytes), pathOrNull, attrs));
-        }
-        _pathOrNull = pathOrNull;
-        _normalizedPathOrNull = normalizedPathOrNull;
-        _pathNameBytes = !isTrailingSlash && attrs.isDirectory() // we could possibly make a defensive copy of it since it's mutable, on the other hand we'd just try not to modify it instead
-                             ? addSlash(pathNameBytes)
-                             : pathNameBytes;
-        _attrs = attrs;
-    }
-
-    // the Receiver must keep all files received from the sender regardless of
-    // any errors to ensure it has a matching file list with the sender
-    public static FileInfo newUntransferrable(byte[] pathNameBytes,
-                                              RsyncFileAttributes attrs)
-    {
-        return new FileInfo(null, null, pathNameBytes, attrs);
-    }
-
-    /**
-     *  not consistent with equals when using windows
-     *  i.e. compareTo result != 0 and equals true is possible,
-     *  but compareTo == 0 implies equals == true
-     */
-    @Override
-    public int compareTo(FileInfo other)
-    {
-        int result = compareUnixFileNamesBytes(_pathNameBytes,
-                                               _attrs.isDirectory(),
-                                               other._pathNameBytes,
-                                               other._attrs.isDirectory());
-        assert result != 0 || this.equals(other);
-        return result;
-    }
-
-    @Override
-    public String toString()
-    {
-        return String.format("%s (attrs=%s, path=%s)",
-                             getClass().getSimpleName(), _attrs, _pathOrNull);
-    }
-
-    // two FileInfo instances are considered equal if the resulting real path is
-    // identical
-    // not consistent with compareTo if running windows see comment for compareTo
-    @Override
-    public boolean equals(Object other)
-    {
-        if (other != null && getClass() == other.getClass()) {
-            FileInfo otherFile = (FileInfo) other;
-            if (_normalizedPathOrNull == null ||
-                otherFile._normalizedPathOrNull == null)
-            {
-                return _normalizedPathOrNull == otherFile._normalizedPathOrNull;
-            }
-            return _normalizedPathOrNull.equals(otherFile._normalizedPathOrNull);
-        } else {
-            return false;
-        }
-    }
-
-    @Override
-    public int hashCode()
-    {
-        return Objects.hash(_normalizedPathOrNull);
-    }
-
-    public RsyncFileAttributes attrs()
-    {
-        return _attrs;
-    }
-
-    public boolean isDotDir()
-    {
-        return _attrs.isDirectory() && isDotDir(_pathNameBytes);
-    }
-
-    // NOTE: may be null for receiver/generator, never null for sender
-    public Path pathOrNull()
-    {
-        return _pathOrNull;
-    }
-
-    /**
-     * WARNING: the result is undefined if the returned array is modified, it
-     * should be considered immutable
-     * @return
-     */
-    public byte[] pathNameBytes()
-    {
-//      return _pathNameBytes.clone();
-        return _pathNameBytes;
-    }
-
-    public boolean isTransferrable()
-    {
-        return _pathOrNull != null;
-    }
-
-    private static byte[] addSlash(byte[] pathNameBytes)
-    {
-        byte[] result = new byte[pathNameBytes.length + 1];
-        System.arraycopy(pathNameBytes, 0, result, 0, pathNameBytes.length);
-        result[result.length - 1] = Text.ASCII_SLASH;
-        return result;
-    }
-
-    private static boolean isDirComponent(byte[] bytes, int offset)
-    {
-        for (int i = offset; i < bytes.length; i++) {
-            if (bytes[i] == Text.ASCII_SLASH) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    private static boolean isDotDir(byte[] bytes)
-    {
-        return bytes.length == 2 &&
-               bytes[0] == Text.ASCII_DOT &&
-               bytes[1] == Text.ASCII_SLASH;          // REDUNDANT if we know it's a directory, we always add slashes to dirs
-    }
-
-    private static int cmpDotDirFirstElseDirAfterFiles(int diff,
-                                                       byte[] leftBytes,
-                                                       boolean isLeftDir,
-                                                       byte[] rightBytes,
-                                                       boolean isRightDir,
-                                                       int offset)
-    {
-        if (isDotDir(leftBytes)) {
-            assert isLeftDir;
-            if (isDotDir(rightBytes)) {
-                assert isRightDir && diff == 0;
-                return 0;
-            }
-            return -1;
-        } else if (isDotDir(rightBytes)) {
-            assert isRightDir;
-            return 1;
-        }
-
-        boolean isLeftADirComponent = isLeftDir ||
-                                      isDirComponent(leftBytes, offset);
-        boolean isRightADirComponent = isRightDir ||
-                                       isDirComponent(rightBytes, offset);
-        if (isLeftADirComponent == isRightADirComponent) {
-            return diff;
-        } else if (isLeftADirComponent) {
-            assert !isRightADirComponent;
-            return 1;
-        } else {
-            assert isRightADirComponent && !isLeftADirComponent;
-            return -1;
-        }
-    }
-
-    private static int cmpSubPaths(boolean isLeftAtEnd, boolean isRightAtEnd)
-    {
-        if (isLeftAtEnd == isRightAtEnd) {
-            return 0;
-        } else if (isLeftAtEnd) {
-            return -1;
-        } // else if (isRightAtEnd) {
-        return 1;
-    }
-
-    /*
-     * sort . (always a dir) before anything else
-     * sort files before dirs on the same level
-     * compare a dir using a trailing slash
-     *
-     * NOTE: this won't sort dot dirs within a path first for that level
-     */
-    private static int compareUnixFileNamesBytes(byte[] leftBytes,
-                                                 boolean isLeftDir,
-                                                 byte[] rightBytes,
-                                                 boolean isRightDir)
-    {
-        int i = 0;
-        for (; i < leftBytes.length && i < rightBytes.length; i++) {
-            int diff = (0xFF & leftBytes[i]) - (0xFF & rightBytes[i]);
-            if (diff != 0) {
-                return cmpDotDirFirstElseDirAfterFiles(diff,
-                                                       leftBytes, isLeftDir,
-                                                       rightBytes, isRightDir,
-                                                       i);
-            }
-        }
-        // one or both are at the end and
-        // one or both are a substring of the other
-        boolean isLeftAtEnd = i == leftBytes.length;
-        boolean isRightAtEnd = i == rightBytes.length;
-        int diff = cmpSubPaths(isLeftAtEnd, isRightAtEnd);
-        return cmpDotDirFirstElseDirAfterFiles(diff, leftBytes, isLeftDir,
-                                               rightBytes, isRightDir, i);
-    }
+    RsyncFileAttributes attrs();
+    String pathName();
 }

yajsync-core/src/main/java/com/github/perlundq/yajsync/attr/LocatableDeviceInfo.java
@@ -16,7 +16,4 @@
  */
 package com.github.perlundq.yajsync.attr;
 
-public interface SymlinkInfo extends FileInfo
-{
-    String targetPathName();
-}
+public interface LocatableDeviceInfo extends DeviceInfo, LocatableFileInfo {}

yajsync-core/src/main/java/com/github/perlundq/yajsync/attr/LocatableFileInfo.java
@@ -16,7 +16,9 @@
  */
 package com.github.perlundq.yajsync.attr;
 
-public interface SymlinkInfo extends FileInfo
+import java.nio.file.Path;
+
+public interface LocatableFileInfo extends FileInfo
 {
-    String targetPathName();
+    Path path();
 }

commit 3812854aa2f281455605bec615b4d82edef58849

FileInfo: compare bytes using unsigned comparison
This regression was introduced in 71af327, causing files to be sorted
wrong if one byte is negative. Fix by performing unsigned comparison (as
was done before 71af327).

Also include a unit test for this.

yajsync-core/src/main/java/com/github/perlundq/yajsync/internal/session/FileInfoImpl.java
@@ -109,6 +109,11 @@ class FileInfoImpl implements FileInfo
         return bytes.length == 1 && bytes[0] == Text.ASCII_DOT;
     }
 
+    private static int cmp(byte a, byte b)
+    {
+        return (0xFF & a) - (0xFF & b);
+    }
+
     /*
      * sort . (always a dir) before anything else
      * sort files before dirs
@@ -137,7 +142,7 @@ class FileInfoImpl implements FileInfo
 
         int i = 0;
         for (; i < leftBytes.length && i < rightBytes.length; i++) {
-            int diff = leftBytes[i] - rightBytes[i];
+            int diff = cmp(leftBytes[i], rightBytes[i]);
             if (diff != 0) {
                 return diff;
             }
@@ -152,9 +157,9 @@ class FileInfoImpl implements FileInfo
             if (isLeftAtEnd && isRightAtEnd) {
                 return 0;
             } else if (isLeftAtEnd) {
-                return Text.ASCII_SLASH - rightBytes[i];
+                return cmp(Text.ASCII_SLASH, rightBytes[i]);
             } else if (isRightAtEnd) {
-                return leftBytes[i] - Text.ASCII_SLASH;
+                return cmp(leftBytes[i], Text.ASCII_SLASH);
             }
         }
 

yajsync-core/src/test/java/com/github/perlundq/yajsync/internal/session/FileInfoTest.java
@@ -194,4 +194,14 @@ public class FileInfoTest {
         assertTrue(f.compareTo(g) <= -1);
     }
 
+    @Test
+    public void testSortUtfString()
+    {
+        String str1 = "Tuabc";
+        String str2 = "Tüabc";
+        FileInfoImpl f = new FileInfoImpl(str1, str1.getBytes(), _fileAttrs);
+        FileInfoImpl g = new FileInfoImpl(str2, str2.getBytes(), _fileAttrs);
+        assertFalse(f.equals(g));
+        assertTrue(f.compareTo(g) <= -1);
+    }
 }